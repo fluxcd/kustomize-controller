b2d19e469f5776b5a0a5a5b4ce150ef67a5d537b
Stefan Prodan <stefan.prodan@gmail.com>
2020-10-09 18:53:49 +0300

    Set ready metric to unknown when condition is missing
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- controllers/kustomization_controller.go
+++ controllers/kustomization_controller.go
@@ -796,6 +796,11 @@ func (r *KustomizationReconciler) recordReadiness(kustomization kustomizev1.Kust
 	}
 	if rc := meta.GetCondition(kustomization.Status.Conditions, meta.ReadyCondition); rc != nil {
 		r.MetricsRecorder.RecordReadyStatus(*objRef, *rc, deleted)
+	} else {
+		r.MetricsRecorder.RecordReadyStatus(*objRef, meta.Condition{
+			Type:   meta.ReadyCondition,
+			Status: corev1.ConditionUnknown,
+		}, deleted)
 	}
 }
 
