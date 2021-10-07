b837b8f629dd9aa537ceb22e4abaa955e5fd1870
Stefan Prodan <stefan.prodan@gmail.com>
2021-10-07 17:52:55 +0300

    Fix flapping kstatus test due to rate limits
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- controllers/kustomization_wait_test.go
+++ controllers/kustomization_wait_test.go
@@ -172,7 +172,7 @@ data:
 		events := getEvents(resultK.GetName(), map[string]string{"kustomize.toolkit.fluxcd.io/revision": revision})
 		g.Expect(len(events) > 0).To(BeTrue())
 		g.Expect(events[len(events)-1].Type).To(BeIdenticalTo("Warning"))
-		g.Expect(events[len(events)-1].Message).To(ContainSubstring("context deadline"))
+		g.Expect(events[len(events)-1].Message).To(ContainSubstring("does-not-exists"))
 	})
 
 	t.Run("recovers and reports healthy status", func(t *testing.T) {
