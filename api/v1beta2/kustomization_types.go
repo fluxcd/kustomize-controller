869ef77fdfed203f1a57a478f6235753d44d52f3
Sunny <darkowlzz@protonmail.com>
2022-02-24 01:47:32 +0530

    api: Move Status in CRD printcolumn to the end
    Status content could be very long compare to other fields. Moving it to
    the end helps improve the visibility of other fields.
    
    Signed-off-by: Sunny <darkowlzz@protonmail.com>

--- api/v1beta2/kustomization_types.go
+++ api/v1beta2/kustomization_types.go
@@ -333,9 +333,9 @@ func (in *Kustomization) GetStatusConditions() *[]metav1.Condition {
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:shortName=ks
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description=""
 // +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].status",description=""
 // +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].message",description=""
-// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description=""
 
 // Kustomization is the Schema for the kustomizations API.
 type Kustomization struct {
