880587f20d4406eaca598b1f6b60d7de4998bcff
Hidde Beydals <hello@hidde.co>
2022-04-20 16:50:49 +0200

    generator: ensure remote build dirs can be reached
    Signed-off-by: Hidde Beydals <hello@hidde.co>

--- controllers/kustomization_controller.go
+++ controllers/kustomization_controller.go
@@ -300,7 +300,7 @@ func (r *KustomizationReconciler) reconcile(
 	revision := source.GetArtifact().Revision
 
 	// create tmp dir
-	tmpDir, err := os.MkdirTemp("", kustomization.Name)
+	tmpDir, err := os.MkdirTemp("", "kustomization-")
 	if err != nil {
 		err = fmt.Errorf("tmp dir error: %w", err)
 		return kustomizev1.KustomizationNotReady(
