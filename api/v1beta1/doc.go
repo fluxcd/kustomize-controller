c8b9d82b7199315c73f9fda171882ae61d9bd92b
Stefan Prodan <stefan.prodan@gmail.com>
2025-08-08 14:43:29 +0300

    Remove deprecated APIs in group `kustomize.toolkit.fluxcd.io/v1beta1`
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- api/v1beta1/doc.go
+++ api/v1beta1/doc.go
@@ -15,6 +15,9 @@ limitations under the License.
 */
 
 // Package v1beta1 contains API Schema definitions for the kustomize v1beta1 API group
+//
+// Deprecated: v1beta1 is no longer supported, use v1 instead.
+//
 // +kubebuilder:object:generate=true
 // +groupName=kustomize.toolkit.fluxcd.io
 package v1beta1
